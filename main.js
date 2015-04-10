cdf345a770b62acc0c463ee1f1e6d63a72f76663
Colin <ccruse@mail.depaul.edu>
2015-04-09 19:00:29 -0500

    working html and jscript

---
+++ main.js
@@ -0,0 +1,31 @@
+$(function (){
+  
+  $("#btn").click(function (){
+                    
+    var from, to, fcountry,tcountry,amt,cnv;
+      
+      
+    fcountry = $("#sourceCurr").val();
+    tcountry = $("#destCurr").val();
+    amt = $("#amount").val();
+        
+    //Only get 1000 api requests a month
+    $.getJSON("https://openexchangerates.org/api/latest.json?app_id=3ebaec61eb9a420f98bfc831e2e960d7",function(json){
+    from = Number(json.rates[fcountry]);
+    to = Number(json.rates[tcountry]);
+    cnv = amt/from*to;
+    console.log(from);
+    console.log(to);
+    console.log(cnv);
+    
+                    
+    });
+    $("Converted Amount").attr("value",cnv);
+                    
+      
+                              
+                              
+    }
+    );
+}
+);
